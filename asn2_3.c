901d4dc8ae659261e8f03e68c62c14b3cf3fc6c6
“deeptabs@gmail.com” <ramitha.d21@gmail.com>
2014-09-16 22:09:40 +0530

    hhhh

--- asn2_3.c
+++ asn2_3.c
@@ -3,13 +3,13 @@
 #include<stdlib.h>
 #include<time.h>
 #include<unistd.h>
-void call(char *fvalue, char *lvalue);
-void compare(char * username, char * password, char *fvalue, char *lvalue);
-char uname[20], pswrd[20];int k=0;
-int flag=0; int i=0;
-char username[20], password[20];
+void call(char *fvalue, char *lvalue, char* c_time_string);
+void compare(char * username, char * password, char *fvalue, char *lvalue, char *c_time_string );
+char uname[20], pswrd[20],login[20],x[10],y[10],z[10];static int k=0;
+int flag=0; int i=0;int l=0;int m=0;int f=0;
+char username[20], password[20];int v=0;
 char buffer[25];char *fvalue= NULL,*lvalue= NULL,lFlag=0;
-char temp[50];char line[100];
+char temp[500];char line[100];
 
 int main(int argc, char **argv)
 {
@@ -58,26 +58,28 @@ int main(int argc, char **argv)
 	filepointer=fopen("log.txt","a+");
 	fprintf(filepointer,"%s\t\tInput Received:%s\n",c_time_string,fvalue);
 	fflush(filepointer);
-	call(fvalue,lvalue);
+	call(fvalue,lvalue,c_time_string);
 	return 0;
 }
 
-void call(char *fvalue, char *lvalue)
+void call(char *fvalue, char *lvalue, char* c_time_string)
 {
 	printf("Please Enter User Name: \n");
 	scanf("%s", username);
 	printf("Please Enter Password: \n");
 	scanf("%s",password);
-	compare(username,password,fvalue,lvalue);
+	compare(username,password,fvalue,lvalue,c_time_string);
      
 
   }
     
-void compare(char * username, char * password, char *fvalue, char *lvalue)
+void compare(char * username, char * password, char *fvalue, char *lvalue, char *c_time_string )
 {	
   int i,j;char line[100];int flag1=0;
   
-  
+		       
+  char* dee;
+ int * dee1;
  FILE *fptr = fopen(fvalue, "r+");
      if (fptr==NULL)
 	{
@@ -99,24 +101,43 @@ if(strcmp(username,usern)==0)
 
 }   fclose(fptr);
 
-FILE *fptr = fopen(fvalue, "r+");
+FILE *fptr = fopen(fvalue, "a+");
 while (!feof(fptr)) //while loop to run while not end of file
-
-          {  i=-1,j=0;
-		       fscanf(fptr,"%s",temp);
+	
+          {  i=-1,j=0,k=0,l=0,m=0;
+		       //fscanf(fptr,"%s",temp);
+		       fgets(temp,500,fptr);
+		       //printf("%s",temp);
+		      
      	       do{
 			     i++;
 		       }while(temp[i]!=',');	
 		       i++;
     		
-		       
+		        f=f+1;
 		      do {  
 			    pswrd[j]=temp[i];
 			    i++;
 			    j++;
-		       } while(temp[i]!='\0');
+		       } while(temp[i]!=',');
 		
 		pswrd[i]='\0';
+		i++;
+		
+		
+		do {  
+			    login[k]=temp[i];
+			    i++;
+			    k++;
+			    //printf("%c",login[k]);
+		       } while(temp[i]!='\0');
+	
+		
+		k++;
+		login[k]='\0';
+		
+		
+		
 		
                                if(flag==1 && strcmp(password,pswrd)==0) 
 
@@ -125,22 +146,88 @@ while (!feof(fptr)) //while loop to run while not end of file
                 }
 	}
 
+
                
 		if(flag1==1)
                {
-		   printf("Welcome %s\n",usern);
+		   printf("welcome %s \n",username);
+		   printf("Last Login : %s\n" ,login);
+		   // printf("Last Login : %s %s %s %s %s \n", temp,dee,x,y,z );
+		   
+		    printf("current login: %s \n",c_time_string);
+		    
+		    fprintf(fptr,"%s,",username);
+		    fprintf(fptr,"%s,",password);
+		    //fwrite(",",sizeof(","),1,fptr);
+		    fprintf(fptr,"%s",c_time_string);
+		    //fwrite("\n",sizeof("\n"),1,fptr);
+		    //printf("line number is %d" ,f);
+                    fclose(fptr); 
+
+
+    FILE *fileptr1,*fileptr2;
+    //char filename[40];
+    char ch;
+    int tp = 1;
+     fileptr1 = fopen("password.txt", "r");
+    ch = getc(fileptr1);
+    //printf("Enter file name: ");
+    //scanf("%s", filename);
+    //open file in read mode
+    
+    ch = getc(fileptr1);
+   while (ch != EOF)
+    {
+        //printf("%c", ch);
+        ch = getc(fileptr1);
+    }
+    //rewind
+    rewind(fileptr1);
+   // printf(" \n Enter line number of the line to be deleted:");
+    //scanf("%d", &delete_line);
+    //open new file in write mode
+    fileptr2 = fopen("replica.txt", "w");
+    ch = getc(fileptr1);
+    while (ch != EOF)
+    {
+        ch = getc(fileptr1);
+        if (ch == '\n')
+            tp++;
+            //except the line to be deleted
+            if (tp != f)
+            {
+                //copy all lines in file replica.c
+                putc(ch, fileptr2);
+            }
+    }
+    fclose(fileptr1);
+    fclose(fileptr2);
+    remove("password.txt");
+    //rename the file replica.c to original name
+    rename("replica.txt", "password.txt");
+    /*printf("\n The contents of file after being modified are as follows:\n");
+    fileptr1 = fopen(filename, "r");
+    ch = getc(fileptr1);
+    while (ch != EOF)
+    {
+        printf("%c", ch);
+        ch = getc(fileptr1);
+    }
+    fclose(fileptr1);
+    return 0;*/
+
 		}
 		else
 			{
-			 k=k+1;
-			if(k==3)
+			 v=v+1;
+			if(v==3)
 				{
 				printf("Maximum attempts exceeded\n");
 				}
 			else
  				{
 				printf("Invalid Username or Password\n you have 3  chances");
-			      call(fvalue,lvalue);
+			      call(fvalue,lvalue,c_time_string);
 			}
 		}
                   
@@ -148,12 +235,50 @@ while (!feof(fptr)) //while loop to run while not end of file
 
                
 
-          }
+          
 
      
 
-     fclose(fptr); 
+    
 
+}
+ 
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                  
+   
+   
+   
